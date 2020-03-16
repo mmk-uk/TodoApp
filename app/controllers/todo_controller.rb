59a20d868a2f79c2ed93144657d6134960346512
mmk-uk <yuuki.muranaka@gmail.com>
2020-03-16 14:23:21 +0900

    ユーザー関連・リストとユーザーの対応付け

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,6 @@
 class TodoController < ApplicationController
   def list
-    @todolist = Todo.all
+    @todolist = Todo.where(user_id: @current_user.id).order(deadline:"ASC")
   end
 
   def new
@@ -11,8 +11,9 @@ class TodoController < ApplicationController
     @todo = Todo.new
     @todo.content = params[:todo][:content]
     @todo.deadline = params[:todo][:deadline]
+    @todo.user_id = @current_user.id
     if @todo.save
-      redirect_to("/")
+      redirect_to("/todo/#{@current_user.id}/list")
     else
       render("todo/new")
     end
@@ -23,7 +24,28 @@ class TodoController < ApplicationController
     todo = Todo.find_by(id: params[:id])
     todo.done = !todo.done
     todo.save
-    redirect_to("/")
+    redirect_to("/todo/#{@current_user.id}/list")
+  end
+
+  def edit
+    @todo = Todo.find_by(id: params[:id])
+  end
+
+  def update
+    @todo = Todo.find_by(id: params[:id])
+    @todo.content = params[:todo][:content]
+    @todo.deadline = params[:todo][:deadline]
+    if @todo.save
+      redirect_to("/todo/#{@current_user.id}/list")
+    else
+      render("todo/new")
+    end
+  end
+
+  def delete
+    @todo = Todo.find_by(id: params[:id])
+    @todo.destroy
+    redirect_to("/todo/#{@current_user.id}/list")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,27 @@
 Rails.application.routes.draw do
-  get "/",to: 'todo#list'
+
+  get "/", to: "home#top"
+
+  get "/todo/:user_id/list",to: 'todo#list'
   get "todo/new",to: 'todo#new'
   post "todo", to: 'todo#create'
   post "todo/:id/check", to:"todo#check"
+  get "todo/:id/edit", to:"todo#edit"
+  patch "todo/:id/update", to:"todo#update"
+  post "todo/:id/delete", to:"todo#delete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  #get    '/users',          to: 'users#index'   # ユーザ一覧ページ
+  #get    '/users/:id',      to: 'users#show'    # 特定のユーザページ
+  #get    '/users/new',      to: 'users#new'     # ユーザ登録ページ
+  #post   '/users',          to: 'users#create'  # ユーザ作成
+  #get    '/users/:id/edit', to: 'users#edit'    # 特定のユーザ編集ページ
+  #patch  '/users/:id',      to: 'users#update'  # 特定のユーザ更新
+  #delete '/users/:id',      to: 'users#destroy' # 特定のユーザ削除
+
+  get "/login", to:"sessions#login_form"
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 end
